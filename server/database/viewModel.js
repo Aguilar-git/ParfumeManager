e7b8e52dae26c7e69fd05f1aa5834836a0d0ee52
Agelar <danell0846@gmail.com>
2021-07-13 20:45:10 +0200

    Добавил несколько запросов.
    /users
    /users-photo
    /user/:id

--- server/database/viewModel.js
+++ server/database/viewModel.js
@@ -7,9 +7,36 @@ function GetLogin(user) {
             password: user.password
         }).select("login");
     }
-    else return undefined;
+    else
+        return undefined;
+}
+
+function GetUsers() {
+    return knex('Users')
+        .join("Brigades", 'Users.brigadeId', 'Brigades.id')
+        .join("Locations", 'Users.locationId', 'Locations.id')
+        .join("Positions", 'Users.positionId', 'Positions.id')
+        .select('Users.id', 'Users.login', 'Users.firstName', 'Users.lastName', 'Users.patronymic', 'Users.dateOfBirth', 'Users.ITN', 'Users.passNumber',
+            'Users.registration', 'Users.dateOfMedicalExamination', 'Users.dateOfFluorography', 'Users.passportScann', 'Users.registrationScann',
+            'Users.userPhoto', { brigade: 'Brigades.name' }, { location: 'Locations.name' }, { position: 'Positions.name' })
+        .where('Users.isHidden', 'false');
+}
+
+function GetUserPhoto(id) {
+    if (id > 0) {
+        return knex("Users").where('Users.id', id).select("Users.userPhoto");
+    }
+    else
+        return null;
+}
+
+function GetUsersPhoto() {
+    return knex("Users").select("Users.userPhoto");
 }
 
 module.exports = {
-    GetLogin
+    GetLogin,
+    GetUsers,
+    GetUserPhoto,
+    GetUsersPhoto,
 }
--- server/server.js
+++ server/server.js
@@ -11,12 +11,22 @@ app.use(express.json());
 
 app.post("/login", async (req, res) => {
     const foundUser = await db.GetLogin(req.body);
+    res.status(200).json(foundUser);
+});
 
-    if (foundUser != undefined) {
-        res.status(200).json(foundUser);
-    } else {
-        res.status(401);
-    }
+app.get("/users", async (req, res) => {
+    const users = await db.GetUsers();
+    res.status(200).json(users);
+});
+
+app.get("/users-photo", async (req, res) => {
+    const usersPhoto = await db.GetUsersPhoto();
+    res.status(200).json(usersPhoto);
+});
+
+app.get("/user/:id", async (req, res) => {
+    const photoURL = await db.GetUserPhoto(req.params.id);
+    res.status(200).json(photoURL);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
