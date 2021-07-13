f019e149f420b6000ca4fcd7f2621e90fd204794
Agelar <danell0846@gmail.com>
2021-07-13 20:54:09 +0200

    Рефакторинг кода

--- server/database/viewModel.js
+++ server/database/viewModel.js
@@ -1,37 +1,55 @@
 const knex = require("./knex");
 
 function GetLogin(user) {
-    if (user.login != undefined && user.password != undefined) {
-        return knex("Users").where({
-            login: user.login,
-            password: user.password
-        }).select("login");
+    try {
+        if (user.login != '' && user.password != '' &&
+            user.login != undefined && user.password != undefined) {
+            return knex("Users").where({
+                login: user.login,
+                password: user.password
+            }).select("login");
+        }
+        else
+            return null;
+    } catch (error) {
+        return error;
     }
-    else
-        return undefined;
 }
 
 function GetUsers() {
-    return knex('Users')
-        .join("Brigades", 'Users.brigadeId', 'Brigades.id')
-        .join("Locations", 'Users.locationId', 'Locations.id')
-        .join("Positions", 'Users.positionId', 'Positions.id')
-        .select('Users.id', 'Users.login', 'Users.firstName', 'Users.lastName', 'Users.patronymic', 'Users.dateOfBirth', 'Users.ITN', 'Users.passNumber',
-            'Users.registration', 'Users.dateOfMedicalExamination', 'Users.dateOfFluorography', 'Users.passportScann', 'Users.registrationScann',
-            'Users.userPhoto', { brigade: 'Brigades.name' }, { location: 'Locations.name' }, { position: 'Positions.name' })
-        .where('Users.isHidden', 'false');
+    try {
+        return knex('Users')
+            .join("Brigades", 'Users.brigadeId', 'Brigades.id')
+            .join("Locations", 'Users.locationId', 'Locations.id')
+            .join("Positions", 'Users.positionId', 'Positions.id')
+            .select('Users.id', 'Users.login', 'Users.firstName', 'Users.lastName', 'Users.patronymic', 'Users.dateOfBirth', 'Users.ITN', 'Users.passNumber',
+                'Users.registration', 'Users.dateOfMedicalExamination', 'Users.dateOfFluorography', 'Users.passportScann', 'Users.registrationScann',
+                'Users.userPhoto', { brigade: 'Brigades.name' }, { location: 'Locations.name' }, { position: 'Positions.name' })
+            .where('Users.isHidden', 'false');
+    } catch (error) {
+        return error;
+    }
 }
 
 function GetUserPhoto(id) {
-    if (id > 0) {
-        return knex("Users").where('Users.id', id).select("Users.userPhoto");
+    try {
+        if (id > 0) {
+            return knex("Users").where('Users.id', id).select("Users.userPhoto");
+        }
+        else
+            return null;
+    } catch (error) {
+        return error;
     }
-    else
-        return null;
 }
 
 function GetUsersPhoto() {
-    return knex("Users").select("Users.userPhoto");
+    try {
+        return knex("Users").select("Users.userPhoto")
+            .where('Users.isHidden', 'false');
+    } catch (error) {
+        return error;
+    }
 }
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -9,21 +9,25 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Находит пользователя по паролю
 app.post("/login", async (req, res) => {
     const foundUser = await db.GetLogin(req.body);
     res.status(200).json(foundUser);
 });
 
+// Возвращает всю информацию о пользователе
 app.get("/users", async (req, res) => {
     const users = await db.GetUsers();
     res.status(200).json(users);
 });
 
+// Возвращает фото всех пользователей
 app.get("/users-photo", async (req, res) => {
     const usersPhoto = await db.GetUsersPhoto();
     res.status(200).json(usersPhoto);
 });
 
+// Возвращает фото пользователя по id
 app.get("/user/:id", async (req, res) => {
     const photoURL = await db.GetUserPhoto(req.params.id);
     res.status(200).json(photoURL);
