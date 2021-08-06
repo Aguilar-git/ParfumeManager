737bb032636daa6338900df2ce58773064a2e4f5
Aguilar <danell0846@gmail.com>
2021-08-06 12:11:30 +0200

    Update viewModel.js

--- server/database/viewModel.js
+++ server/database/viewModel.js
@@ -20,13 +20,12 @@ function GetLogin(user) {
 // Возвращает всю информацию о пользователях
 function GetUsers() {
     try {
-        return knex('Users')
+        return knex('Users').select('Users.id', 'Users.login', 'Users.firstName', 'Users.lastName', 'Users.patronymic', 'Users.dateOfBirth', 'Users.ITN', 'Users.passNumber',
+            'Users.registration', 'Users.dateOfMedicalExamination', 'Users.dateOfFluorography', 'Users.passportScann', 'Users.registrationScann',
+            'Users.userPhoto', { brigade: 'Brigades.name' }, { location: 'Locations.name' }, { position: 'Positions.name' })
             .join("Brigades", 'Users.brigadeId', 'Brigades.id')
             .join("Locations", 'Users.locationId', 'Locations.id')
             .join("Positions", 'Users.positionId', 'Positions.id')
-            .select('Users.id', 'Users.login', 'Users.firstName', 'Users.lastName', 'Users.patronymic', 'Users.dateOfBirth', 'Users.ITN', 'Users.passNumber',
-                'Users.registration', 'Users.dateOfMedicalExamination', 'Users.dateOfFluorography', 'Users.passportScann', 'Users.registrationScann',
-                'Users.userPhoto', { brigade: 'Brigades.name' }, { location: 'Locations.name' }, { position: 'Positions.name' })
             .where('Users.isHidden', 'false');
     } catch (error) {
         return error;
